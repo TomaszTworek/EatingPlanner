204f128db316578d0f27a947ae3cea431e4b995e
Tomasz Tworek <tomasztworek27@gmail.com>
2022-02-21 18:17:11 +0100

    Initial commit - backend project

---
+++ EatingPlanner/src/main/java/pl/tworek/EatingPlanner/EatingPlannerApplication.java
@@ -0,0 +1,13 @@
+package pl.tworek.EatingPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatingPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatingPlannerApplication.class, args);
+    }
+
+}
---
+++ EatingPlanner/src/test/java/pl/tworek/EatingPlanner/EatingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tworek.EatingPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatingPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
